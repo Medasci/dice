60e89af35fbb37c493f35e174ef2bac43a814e85
Medasci <mehmetd@metropolia.fi>
2022-10-26 08:37:25 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import zero from './img/dice-empty.png'
+import img1 from './img/dice1.png'
+import img2 from './img/dice2.png'
+import img3 from './img/dice3.png'
+import img4 from './img/dice4.png'
+import img5 from './img/dice5.png'
+import img6 from './img/dice6.png'
+
+
+const Dice = () => {
+
+    const diceImages = [zero, img1, img2, img3, img4, img5, img6]
+    const [dicImg, setDiceImg] = useState(zero)
+
+    useEffect(() => {
+        if (dicImg === zero) {
+            setTimeout(diceRoll, 1000)
+        }
+
+    });
+
+    const diceRoll = () => {
+        let randomNumber = Math.floor(Math.random() * 6 );
+        console.log(randomNumber);
+
+        setDiceImg(diceImages[randomNumber])
+
+    }
+
+    const diceHandler = () => {
+        setDiceImg(zero)
+    }
+
+    return (
+        <img className='Dice' onClick={diceHandler} src={dicImg} alt='' />
+    )
+
 }
 
-export default App;
+export default Dice;
